505f2840c041495b7ef23e7eb333cd1b9ce03cf0
Tomasz Kolek <tomasz-kolek@o2.pl>
2016-04-27 23:07:12 +0200

    changed script to insert data to mongo DB

--- Skrypty z RPI/DHT11Pomiar.py
+++ Skrypty z RPI/DHT11Pomiar.py
@@ -1,36 +1,45 @@
 #!/usr/bin/python
 
 import sys
+from datetime import datetime
 import Adafruit_DHT
-import time
-import MySQLdb
+import pymongo
 
-#Polaczenie z baza danych MySQL
-def insert_temp_reading (temperature,humidity):
- 
- conn = MySQLdb.connect("host","nazwa_użtkownika","hasło","nazwa_bazy_danych")
- cursor = conn.cursor()
- params = [temperature,humidity]
- 
- try:
-   cursor.execute("INSERT INTO dane (data,temperatura,wilgotnosc) VALUE (NOW(),%s,%s)",params)
-   conn.commit()
- except MySQLdb.Error, e:
-   print "An error has occurred. %s" %e
- finally:
-  cursor.close()
-  conn.close() 
+
+def get_database_instance_ready_to_work():
+    """
+    Creates connection to MongoDb
+    """
+    client = pymongo.MongoClient('127.0.0.1:3001')
+    return client.mongo
+
+
+def read_humidity_and_temperature_from_raspberry():
+    """
+    Read from sensor DHT17 connected to GPIO17
+    """
+    return Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 17)
+
+
+def insert_data(temperature, humidity):
+    """
+    Inserts temperature and humidity to separated collections in Mongo
+    """
+    db = get_database_instance_ready_to_work()
+    data_read_date = datetime.now()
+    db.temperature.insert_one(dict(temperature=temperature, date=data_read_date))
+    db.humidity.insert_one(dict(humidity=humidity, date=data_read_date))
+
 
 def main():
-#Pobieranie temperatury oraz wilgotnosci z sensora DHT11 podpietego do GPIO17
-	humi, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 17)
-#Wyswietlanie temperatury i zapis do bazy danych MySQL
-	if humi is not None and temp is not None:
-		insert_temp_reading (temp,humi)
-		print('Temperatura={0:0.1f}* Wilgotnosc={1:0.1f}%'.format(temp, humi))
-	else:
-		print('Blad odczytu. Sprobuj ponownie!')
+    humidity, temperature = read_humidity_and_temperature_from_raspberry()
+    if humidity is not None and temperature is not None:
+        insert_data(temperature, humidity)
+        print('Temperature={0:0.1f}*\nHumidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Read data error. Try again!')
+        sys.exit(1)
 
-		sys.exit(1)
+
 if __name__ == '__main__':
     main()
